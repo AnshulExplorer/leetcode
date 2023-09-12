81e6b8f165a8ae540dc1cc7219c71bfd83eb520a
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2023-09-13 02:11:53 +0530

    Time: 0 ms (100%), Space: 6.2 MB (44.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<=3)return n;
-        int a=2;
-        int b=3;
+        vector<int>nums{1,2,3};
+        if(n<4)return n;
         for(int i=3;i<n;i++){
-            int temp=a+b;
-            a=b;
-            b=temp;
+            nums.push_back(nums[i-1]+nums[i-2]);
         }
-      return b;
+        return nums[n-1];
     }
 };
