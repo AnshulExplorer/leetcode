9f0785a97885f52a7b1087549772319cf1602e53
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2023-09-18 00:04:05 +0530

    Time: 0 ms (100%), Space: 6.4 MB (16.59%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
+int go(int n){
+    if(n==0)return 0;
+    if(n % 2==0){
+        return 1 + go(n/2);
+    }
+    return 1 + go(n-1); 
+}
     int numberOfSteps(int num) {
-        int count=0;
-        if(num==0)return 0;
-        if(num % 2==0){
-           return 1 +  numberOfSteps(num/2);
-        }
-        else {
-            return 1 +  numberOfSteps(num-1);
-        }
+       return go(num);
     }
 };
