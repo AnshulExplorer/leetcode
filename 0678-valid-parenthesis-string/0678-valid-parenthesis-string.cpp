56398053db6ed232bd160c30274eb6a74918939e
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-04-07 18:01:35 +0530

    Time: 0 ms (100%), Space: 7.1 MB (92.84%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int cnt=0;
+        int cntstar=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(')cnt++;
+            else if(s[i]=='*')cntstar++;
+            else if(s[i]==')'){
+                if(cnt==0){
+                    if(cntstar>0){
+                        cntstar--;
+                    }
+                    else return false;
+                }
+                else{
+                    cnt--;
+                }
+            }
+        }
+        cnt=0;
+        cntstar=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==')')cnt++;
+            else if(s[i]=='*')cntstar++;
+            else if(s[i]=='('){
+                if(cnt==0){
+                    if(cntstar>0){
+                        cntstar--;
+                    }
+                    else return false;
+                }
+                else cnt--;
+            }
+            
+        }
+        return true;  
+    }
+};
