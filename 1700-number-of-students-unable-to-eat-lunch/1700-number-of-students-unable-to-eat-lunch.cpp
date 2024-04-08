8e8424aa8136c4c5d6ed351c43cf205aa93af8b7
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-04-08 17:31:44 +0530

    Time: 5 ms (31.29%), Space: 10.7 MB (79.46%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int ans=0;
-        int i=0;
-        for(int j=0;j<sandwiches[j];j++){
-           while(students[i]){
-                if(sandwiches[j]==students[i]){
-                    ans++;
-                    students[i]=-1;   
-                }
-                i++;
-           }
+        int student_total=students.size(),cir=0,sq=0;
+        for(int i=0;i<students.size();i++){
+            if(students[i]==0)cir++;
+            else sq++;
         }
-        return ans;
+        for(int i=0;i<sandwiches.size();i++){
+            if(sandwiches[i]==0){
+                if(cir>0){
+                    cir--;
+                    student_total--;
+                }
+                else {
+                    return student_total;
+                }
+            }
+            else{
+               if(sq>0){
+                sq--;
+                student_total--;
+               } 
+               else{
+                return student_total;
+               }
+            }
+        }
+        return 0;
     }
 };
