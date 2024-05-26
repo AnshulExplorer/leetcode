56cbb7851e0793ea9c5451d0e82cef5f4b82b701
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-05-27 01:17:19 +0530

    Time: 22 ms (46.13%), Space: 28.5 MB (5.97%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -7,13 +7,13 @@ public:
         }
         vector<int>ans;
         for(int i=0;i<n;i++){
-            ans.push_back(nums[i]);    //1 2 3 4 5 6 7
+            ans.push_back(nums[i]);
         }
         for(int i=0;i<k;i++){
-            nums[i]=ans[n-k+i];   // 5 6 7
-        }
+            nums[i]=ans[n-k+i];
+        }//5 6 7
         for(int i=0;i<n-k;i++){
-            nums[k+i]=ans[i];
+            nums[i+k]=ans[i];
         }
         return ;
     }
