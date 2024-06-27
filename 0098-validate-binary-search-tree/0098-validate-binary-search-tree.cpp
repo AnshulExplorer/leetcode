33212eb6bd38a2c0510815680c57997089d1ef5d
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-06-27 14:05:32 +0530

    Time: 8 ms (56.77%), Space: 20.2 MB (61.22%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int maxTree(TreeNode* root){
-        if(root==NULL)return INT_MIN;
-        return max(root->val,max(maxTree(root->left),maxTree(root->right)));
+    long long maxTree(TreeNode* root){
+        if(root==NULL)return LLONG_MIN;
+        return max((long long)(root->val),max(maxTree(root->left),maxTree(root->right)));
     }
-    int minTree(TreeNode* root){
-        if(root==NULL)return INT_MAX;
-        return min(root->val,min(minTree(root->left),minTree(root->right)));
+    long long minTree(TreeNode* root){
+        if(root==NULL)return LLONG_MAX;
+        return min((long long)(root->val),min(minTree(root->left),minTree(root->right)));
     }
     bool isValidBST(TreeNode* root) {
         if(root==NULL)return true;
-        else if(root->val <= maxTree(root->left))return false;
-        else if(root->val >= minTree(root->right))return false;
-        return isValidBST(root->left) || isValidBST(root->right);
+        else if((long long)(root->val) <= maxTree(root->left))return false;
+        else if((long long)(root->val) >= minTree(root->right))return false;
+        return isValidBST(root->left) && isValidBST(root->right);
     }
 };
