f0ea93aaa2d1aeb2f926bfd6d6bd04d6725704b9
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-07-05 16:02:02 +0530

    Time: 63 ms (53.59%), Space: 56 MB (52.89%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -1,26 +1,26 @@
 
 class Solution {
 public:
-void insert(TreeNode*root,int val){
-    if(root==NULL)root=new TreeNode(val);
-     else if(root->val > val){   //left
-        if(root->left==NULL){
-            TreeNode*temp=new TreeNode(val);
-            root->left=temp;
-        }
-        else insert(root->left,val);
+    void helper(TreeNode*root,int val){
+       if(root==NULL)root=new TreeNode(val);
+       else if(root->val > val){
+            if(root->left==NULL){
+                TreeNode*temp=new TreeNode(val);
+                root->left=temp;
+            }
+            else helper(root->left,val);
+       }
+       else {   // root->val < val
+            if(root->right==NULL){
+                TreeNode*temp=new TreeNode(val);
+                root->right=temp;
+            }
+            else helper(root->right,val);
+       }
     }
-    else {    //right
-        if(root->right==NULL){
-            TreeNode*temp=new TreeNode(val);
-            root->right=temp;
-        }
-        else insert(root->right,val);
-    }
-}
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        insert(root,val);
-        if(root==NULL)root=new TreeNode(val);
+        helper(root,val);
+        if(root==NULL)root= new TreeNode(val);
         return root;
     }
 };
