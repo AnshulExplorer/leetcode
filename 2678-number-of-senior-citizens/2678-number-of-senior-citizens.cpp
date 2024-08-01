63ff2ef94f1e894ce45bd86ac9ddffb4002ab876
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-08-01 23:44:17 +0530

    Time: 6 ms (60.14%), Space: 17.6 MB (16.71%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count=0;
+        for(int i=0;i<details.size();i++){
+            int a=(details[i][11]-'0')*10 + (details[i][12]-'0');  //imp
+            if(a>60)count++;
+        }
+        return count;
+    }
+};
