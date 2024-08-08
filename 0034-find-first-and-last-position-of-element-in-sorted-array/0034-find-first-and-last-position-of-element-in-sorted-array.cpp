357f2e40bd1d5b357a96f1bd1ded1730b8261634
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-08-09 01:52:42 +0530

    Time: 17 ms (11.92%), Space: 13.7 MB (100%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int s=-1;
-                int e=-1;
-                        for(int i=0;i<nums.size();i++){
-                                    if(nums[i]==target){
-                                                    s=i;
-                                                                    break;
-                                                                                }
-                                                                                        }
-                                                                                                for(int j=nums.size()-1;j>=0;j--){
-                                                                                                            if(nums[j]==target){
-                                                                                                                            e=j;
-                                                                                                                                            break;
-                                                                                                                                                        }
-                                                                                                                                                                }
-                                                                                                                                                                        return {s,e};
-    }
+        int n=nums.size();
+        int start=-1;
+        int end=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                start=i;
+                break;
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]==target){
+                end=i;
+                break;
+            }
+        }
+        return {start,end};
+    } 
 };
