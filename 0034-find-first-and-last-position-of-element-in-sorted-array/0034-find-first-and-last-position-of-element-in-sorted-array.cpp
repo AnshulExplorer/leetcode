1e0eab14453ddfb81f84b3121b7e84c37075c21d
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-08-09 01:00:24 +0530

    Time: 0 ms (100%), Space: 16.3 MB (22.26%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int s=-1;
-        int e=-1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                s=i;
-                break;
-            }
-        }
-        for(int j=nums.size()-1;j>=0;j--){
-            if(nums[j]==target){
-                e=j;
-                break;
-            }
-        }
-        return {s,e};
- 
+                int e=-1;
+                        for(int i=0;i<nums.size();i++){
+                                    if(nums[i]==target){
+                                                    s=i;
+                                                                    break;
+                                                                                }
+                                                                                        }
+                                                                                                for(int j=nums.size()-1;j>=0;j--){
+                                                                                                            if(nums[j]==target){
+                                                                                                                            e=j;
+                                                                                                                                            break;
+                                                                                                                                                        }
+                                                                                                                                                                }
+                                                                                                                                                                        return {s,e};
     }
 };
