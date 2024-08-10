4993713973d69ba04e4b9dcf181dc41082cf4d6b
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-08-11 00:51:03 +0530

    Time: 0 ms (100%), Space: 7.8 MB (13.31%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int s1=0,s2=0;
+        int preSum=0;
+        int postSum=n*(n+1)/2;  // 36
         for(int i=1;i<=n;i++){
-            s1+=i;           // 1 to n
-        }
-        for(int i=1;i<=n;i++){
-            s2+=i;
-            if(s1==s2){
-                return i;
-            }
-            s1=s1-i;
+            preSum+=i;
+            if(preSum==postSum)return i;
+            postSum-=i;
         }
         return -1;
     }
