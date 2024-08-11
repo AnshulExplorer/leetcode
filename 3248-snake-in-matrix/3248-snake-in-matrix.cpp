7ae63d440b0216143991c43ed9b999c69be0744c
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-08-11 14:09:09 +0530

    Time: 20 ms (50%), Space: 35.1 MB (90%) - LeetHub

--- 3248-snake-in-matrix/3248-snake-in-matrix.cpp
+++ 3248-snake-in-matrix/3248-snake-in-matrix.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int finalPositionOfSnake(int n, vector<string>& commands) {
-        // vector<vector<int>>ans(n,vector<int>(n));
-        // int a=0;
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         ans[i][j]=a;
-        //         a++;
-        //     }
-        // }
        int a=0;
        for(int i=0;i<commands.size();i++){
         if(commands[i]=="RIGHT")a=a+1;
