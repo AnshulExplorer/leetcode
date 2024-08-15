d7a4c17b80cac16868f394f56f2149f2fecfc4ea
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-08-15 18:53:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int ten=0,five=0;
+        int n=bills.size();
+        if(bills[0]==10 || bills[0]==20)return false;
+        for(int i=1;i<n;i++){
+            if(bills[i]==5)five++;
+            else if(bills[i]==10){
+                if(five>0){
+                    five--;
+                    ten++;
+                }
+                else return false;
+            }
+            else if(bills[i]==20){            //for 20
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
