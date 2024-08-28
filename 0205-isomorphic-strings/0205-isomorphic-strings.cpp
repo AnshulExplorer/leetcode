26ab5c70d3582e9c9ccdb1aa643b2af38f814904
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-08-29 00:36:32 +0530

    Time: 10 ms (17.89%), Space: 9.5 MB (44.69%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size()!=t.size())return false ;
-        vector<int>v(150,1000);
+        if(s.size()!=t.size())return false;
+        unordered_map<char,int>mp;
+        unordered_map<char,int>mp2;
         for(int i=0;i<s.size();i++){
-            int idx=(int)s[i];
-            if(v[idx]==1000) v[idx]=s[i]-t[i];
-            else if(v[idx]!=(s[i]-t[i]))return false ;
+            if(mp[s[i]]!=mp2[t[i]])return false;
+            mp[s[i]]=i+1;
+            mp2[t[i]]=i+1;
         }
-        //emptying the vector
-
-        for(int i=0;i<150;i++)
-            v[i]=1000;
-        for(int i=0;i<t.size();i++){
-            int idx=(int)t[i];
-            if(v[idx]==1000) v[idx]=t[i]-s[i];
-            else if(v[idx]!=(t[i]-s[i]))return false ;
-        } 
-          return true;                                                          
-    }   
+        return true;
+    }
 };
