49cb2c09db0ccdf338a25fe67a8944b602e8421d
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-10-15 11:45:43 +0530

    Time: 37 ms (25.68%), Space: 15.4 MB (89.89%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long ans=0;
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            while(i<j && s[i]=='0')i++;
+            while(i<j && s[j]=='1')j--;
+            if(i<j){
+                ans+=(j-i);
+                i++,j--;
+            }
+        }
+        return ans;
+    }
+};
