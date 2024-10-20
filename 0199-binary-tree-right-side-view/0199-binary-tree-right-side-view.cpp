2670b0d807e0760dfe5826b3f22c0577b6a57719
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-10-20 17:23:51 +0530

    Time: 0 ms (100%), Space: 14.5 MB (34.59%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,28 +1,33 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-int levels(TreeNode*root){
-    if(root==NULL)return 0;
-    return 1 + max(levels(root->left),levels(root->right));
-}
-void nthlevel(TreeNode*root,int curr,int level,vector<int>&ans){
-    if(root==NULL)return ;
-    if(curr==level){
-        ans[curr]=root->val;
-        return ;
-    }
-    nthlevel(root->left,curr+1,level,ans);
-    nthlevel(root->right,curr+1,level,ans);
-}
-    void levelsOrder(TreeNode*root,vector<int>&ans){
-        int n=ans.size();
-        for(int i=0;i<n;i++){
-            nthlevel(root,0,i,ans);
-        }
-    }
     vector<int> rightSideView(TreeNode* root) {
-        vector<int>ans(levels(root),0);
-        levelsOrder(root,ans);
+        vector<int>ans;
+        if(root==NULL)return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int s=q.size();
+            while(s--){
+                TreeNode* a=q.front();
+                q.pop();
+                if(s==0){
+                    ans.push_back(a->val);
+                }
+                if(a->left)q.push(a->left);
+                if(a->right)q.push(a->right);
+            }
+        }
         return ans;
     }
 };
