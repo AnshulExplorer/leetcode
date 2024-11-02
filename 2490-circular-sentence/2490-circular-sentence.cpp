2e22886c5eb78f5c08821798dd2a91cff351c7f8
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-11-02 22:51:57 +0530

    Time: 3 ms (10%), Space: 9.1 MB (16.96%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.cpp
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    bool isCircularSentence(string s) {
-        vector<string>st;
-        string a="";
-        for(int i=0;i<s.size();i++){
-           if(s[i]!=' '){
-               a+=s[i]; 
-           }
-           else{
-             st.push_back(a);
-             a="";
-           }
+    bool isCircularSentence(string sentence) {
+        vector<string>v;
+        string s="";
+        for(int i=0;i<sentence.size();i++){
+            if(sentence[i]!=' '){
+                s+=sentence[i];
+            }
+            else{
+                v.push_back(s);
+                s="";
+            }
         }
-        if(a.size()!=NULL)st.push_back(a);
-        // leetcode exercise sound delightful
-        int siz=st.size();
-        if(siz==1){
-            return st[0][0]==st[0].back();
-        }
-        if(st[0][0]!=st[siz-1].back())return false;
-        for(int i=0;i<st.size()-1;i++){
-            if(st[i].back()!=st[i+1][0])return false;
+        if(s.size()!=NULL)v.push_back(s);
+        if(v[0][0]!=v[v.size()-1].back())return false;
+        for(int i=0;i<v.size()-1;i++){
+            if(v[i].back()!=v[i+1][0])return false;
         }
         return true;
-        
     }
 };
