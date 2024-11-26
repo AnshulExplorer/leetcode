aaa72a1fc7faace092086b88741fb53c1cb3ce67
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2024-11-26 08:40:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3365-rearrange-k-substrings-to-form-target-string/3365-rearrange-k-substrings-to-form-target-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+void help(unordered_map<string,int>&mp,string &s,int k){
+    int n=s.size();
+    int p=n/k;
+    int ans=0;
+    while(k>0){
+        string a=s.substr(ans,p);
+        mp[a]++;
+        ans+=p;
+        k--;
+    }
+}
+    bool isPossibleToRearrange(string s, string t, int k) {
+        unordered_map<string,int>mp;
+        unordered_map<string,int>mp2;
+        if(s.size()!=t.size())return false;
+        help(mp,s,k);
+        help(mp2,t,k);
+        for(auto x:mp){
+            if(mp2.find(x.first)==mp2.end())return false;
+        }
+        return true;
+    }
+};
