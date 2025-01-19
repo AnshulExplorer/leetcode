c5ff8b60d21c93571f0fe86ece110783a6b997bf
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2025-01-19 18:30:19 +0530

    Time: 118 ms (8.24%), Space: 32.6 MB (26.56%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int n=s.size();
+        int n=s.size(),ans=0;
         map<char,int>mp;
         for(auto x:s)mp[x]++;
-        if(n>2){
-            for(int i=1;i<n-1;i++){
-                if(mp.find(s[i])!=mp.end()){
-                    while(mp[s[i]]>2){
-                        mp[s[i]]--;
-                        mp[s[i]]--;
-                        n-=2;
-                    }
-                }
+        for(auto x:mp){
+            if(x.second%2==0){
+                ans+=2;
             }
+            else ans++;
         }
-        return n;
+        return ans;
     }
 };
