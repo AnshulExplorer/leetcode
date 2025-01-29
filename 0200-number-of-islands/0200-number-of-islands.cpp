7984aeffb74170d6a537eaa20312a0a33c070731
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2025-01-29 17:38:16 +0530

    Time: 22 ms (88.55%), Space: 16.3 MB (77.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-void mark_visited(vector<vector<char>> &grid,int i,int j,int row,int col){
-    if(i<0 || i>=row || j<0 ||j>=col || grid[i][j]!='1'){
-        return ;
+    void dfs(int i,int j,vector<vector<char>>& grid){
+        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]=='0'){
+            return ;
+        }
+        grid[i][j]='0';
+        dfs(i+1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
     }
-    grid[i][j]='2'; //mark as visited
-    mark_visited(grid,i+1,j,row,col);  //down
-    mark_visited(grid,i,j+1,row,col);  //right
-    mark_visited(grid,i-1,j,row,col);  //up
-    mark_visited(grid,i,j-1,row,col);  //left
-}
     int numIslands(vector<vector<char>>& grid) {
         int ans=0;
-        int row=grid.size();
-        int col=grid[0].size();
-        if(row==0)return 0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]=='1'){
-                    mark_visited(grid,i,j,row,col);
+        int m=grid.size(),n=grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]!='0'){
+                    dfs(i,j,grid);
                     ans++;
                 }
             }
