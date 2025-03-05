116a31772472e8856f83cc7e720d88c8d09abb1f
Anshul Dhiman <140399225+AnshulExplorer@users.noreply.github.com>
2025-03-05 23:45:10 +0530

    Time: 0 ms (100%), Space: 7.9 MB (31.17%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    long long coloredCells(int n) {
-        // long long ans=n*n+(n-1)*(n-1);
-        return 1LL * n * n + 1LL * (n - 1) * (n - 1);
+    long long coloredCells(long long n) {
+        long long ans=n*n;
+        long long ans2=(n-1)*(n-1);
+        return ans+ans2;
     }
 };
